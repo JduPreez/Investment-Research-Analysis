1a4acc5c1e622bfc10b1fdb3c35add888698c114
JDuPreez <https://medium.com/@recursiveenigma>
2019-11-28 10:12:57 +0200

    Rescaled Range.R now calculates the cumulative deviate series

--- Stocks/Rescaled Range.R
+++ Stocks/Rescaled Range.R
@@ -1,3 +1,6 @@
+#install.packages(c("TTR"))
+#library(TTR)
+
 # Rescaled Range:
 # https://everything.explained.today/Rescaled_range/
 # Implementation in Python: https://github.com/Mottl/hurst
@@ -22,10 +25,17 @@ mean_adj_ser <- function(values, mean_val)  {
   mean_adj_ser <- c() # An empty vector   
 
   for (i in 1:length(values)) {
-    mean_adj_ser[i] <- values[i] - mean_val
+    mean_adj_ser[i] <- round(values[i] - mean_val, 6)
   }
   mean_adj_ser
 }
 
 mas <- mean_adj_ser(closePrices, m)
 mas
+
+# 3. Calculate the cumulative deviate series Z
+# --------------------------------------------
+# Take the first value X, then sum of the first two values X+Y, then sum of the first 
+# three values X+Y+Z and so on to create a new series.
+cds <- cumsum(mas) # runSD(mas, n=1, cumulative=TRUE)
+cds
